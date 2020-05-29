e7c4a7f84e698f60ad9320200c144560206792e7
k-wasilewski <k.k.wasilewski@gmail.com>
2020-05-29 12:44:14 +0200

    newItemReducer.test passed

--- tests/redux/newItemReducer.test.js
+++ tests/redux/newItemReducer.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import * as actions from "../../src/redux/actions"
 import newItemReducer from "../../src/redux/newItemReducer";
+import {addItem} from "../../src/redux/actions";
 
 describe("newItemReducer specification", () => {
     it('should return the initial state', () => {
         expect(newItemReducer(undefined, {})).toEqual({
             item: []
+        })
+    })
+
+    it('should handle addItem', () => {
+        const item = 'sample item'
+        expect(newItemReducer([], actions.addItem(item))).toEqual({
+            item: item
         })
     })
 })
