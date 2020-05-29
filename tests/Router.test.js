6cae25a639acd130c84aa45d80f1365e597cc0e6
k-wasilewski <k.k.wasilewski@gmail.com>
2020-05-29 11:58:55 +0200

    Router.test passed

--- tests/Router.test.js
+++ tests/Router.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {MemoryRouter} from "react-router-dom";
 import NewOrderStep1 from "../src/components/orders/NewOrderStep1";
+import Orders from "../src/components/orders/Orders";
+import Summary from "../src/components/summary/Summary"
 import store from "../src/redux/store";
 import {Provider} from "react-redux";
 import App from "../src/App";
@@ -23,5 +25,39 @@ describe("Router specification", () => {
         );
 
         expect(component.find(NewOrderStep1)).toHaveLength(1);
+    })
+
+    it('component "Orders" is displayed when "/step2" url is passed', () => {
+        configure({ adapter: new Adapter() });
+
+        const root = document.createElement('div');
+        document.body.appendChild(root);
+
+        const component = mount(
+            <Provider store={store}>
+                <MemoryRouter initialEntries={['/step2']}>
+                    <App />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        expect(component.find(Orders)).toHaveLength(1);
+    })
+
+    it('component "Summary" is displayed when "/summary" url is passed', () => {
+        configure({ adapter: new Adapter() });
+
+        const root = document.createElement('div');
+        document.body.appendChild(root);
+
+        const component = mount(
+            <Provider store={store}>
+                <MemoryRouter initialEntries={['/summary']}>
+                    <App />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        expect(component.find(Summary)).toHaveLength(1);
     })
 })
