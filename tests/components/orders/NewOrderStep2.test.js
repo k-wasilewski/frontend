827bec6e13e455b499cd1711aeb118ff7f85e68f
k-wasilewski <k.k.wasilewski@gmail.com>
2020-05-30 19:56:04 +0200

    NewOrderStep2.test render

--- tests/components/orders/NewOrderStep2.test.js
+++ tests/components/orders/NewOrderStep2.test.js
@@ -7,7 +7,7 @@ import Adapter from "enzyme-adapter-react-16";
 import ConnectedNewOrderStep2, { NewOrderStep2 } from "../../../src/components/orders/NewOrderStep2";
 
 describe("NewOrderStep2 specification", () => {
-    it('should render zxczxczxDDSSDs', () => {
+    it('should render a form with select, radio, and two buttons', () => {
         const component = renderer.create(
             <Provider store={store}>
                 <ConnectedNewOrderStep2 />
@@ -16,7 +16,38 @@ describe("NewOrderStep2 specification", () => {
         let tree = component.toJSON();
         expect(tree).toMatchSnapshot();
 
+        const form = tree.children[0]
+        expect(form.type).toEqual('form')
 
+        const col1 = form.children[1].children[0]
+
+        const btn1 = col1.children[5]
+        expect(btn1.children[0]).toEqual('Zapisz')
+        const btn2 = col1.children[6]
+        expect(btn2.children[0]).toEqual('Wyślij')
+
+        const col2 = form.children[1].children[1]
+
+        const select = col2.children[0].children[0]
+        expect(select.props).toHaveProperty('id', 'color' )
+        expect(select.props).toHaveProperty('name', 'color' )
+
+        const radio1 = col2.children[1]
+        expect(radio1.props).toHaveProperty('id', 's' )
+        expect(radio1.props).toHaveProperty('type', 'radio' )
+        expect(radio1.props).toHaveProperty('name', 'size' )
+        const radio4 = col2.children[4]
+        expect(radio4.props).toHaveProperty('id', 'm' )
+        expect(radio4.props).toHaveProperty('type', 'radio' )
+        expect(radio4.props).toHaveProperty('name', 'size' )
+        const radio7 = col2.children[7]
+        expect(radio7.props).toHaveProperty('id', 'l' )
+        expect(radio7.props).toHaveProperty('type', 'radio' )
+        expect(radio7.props).toHaveProperty('name', 'size' )
+        const radio10 = col2.children[10]
+        expect(radio10.props).toHaveProperty('id', 'xl' )
+        expect(radio10.props).toHaveProperty('type', 'radio' )
+        expect(radio10.props).toHaveProperty('name', 'size' )
     })
 
     it('sdfsdfsd', () => {
