436101607a8d68e401cfe52abc4657db954b8dae
k-wasilewski <k.k.wasilewski@gmail.com>
2020-07-17 09:56:54 +0200

    extended TranslateItems.test and UntranslateItems.test

--- tests/func/UntranslateItems.test.js
+++ tests/func/UntranslateItems.test.js
@@ -4,11 +4,11 @@ import UntranslateItems from "../../src/func/UntranslateItems";
 describe("UntranslateItems functional specification", () => {
     it('TranslateItems() translates color and size to polish', () => {
         const items = [{id: 0, color: 'Niebieski', size: 'S'},
-            {id: 1, color: 'Błękitny', size: 'XL'}];
+            {id: 1, color: 'Błękitny', size: 'XL'}, {id: 2, color: 'Granatowy', size: 'L'}];
 
         UntranslateItems(items)
 
         expect(items).toEqual([{id: 0, color: 'blue', size: 's'},
-            {id: 1, color: 'lightblue', size: 'xl'}]);
+            {id: 1, color: 'lightblue', size: 'xl'}, {id: 2, color: 'darkblue', size: 'l'}]);
     });
 });
